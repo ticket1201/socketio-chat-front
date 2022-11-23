8831693bb98512e327d9df6a69358e3b48d98831
Anton Metlitsky <ticket1201@gmail.com>
2022-11-23 20:24:43 +0400

    feat: impl basic websocket chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {io} from 'socket.io-client';
+
+const socket = io('https://socketio-chat-back-production.up.railway.app')
+//const socket = io('http://192.168.0.104:3009')
+
+type UserType = {
+    id: string
+    name: string
+}
+
+type MessagesType = {
+    message: string
+    id: string
+    user: UserType
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [messages, setMessages] = useState<Array<MessagesType>>([])
+    const [users, setUsers] = useState<Array<string>>([])
+    const [text, setText] = useState('')
+    const [name, setName] = useState('')
+
+    useEffect(() => {
+        socket.on('init-messages-published', (messages: Array<MessagesType>) => {
+            setMessages(messages)
+        })
+        socket.on('new-message-sent', (message: MessagesType) => {
+            setMessages((pred) => [...pred, message])
+        })
+        socket.on('new-name-sent', (name: string) => {
+            setName(name)
+        })
+        socket.on('user-sent', (names: Array<string>) => {
+            setUsers(names)
+        })
+    }, [])
+
+
+    return !name
+        ? (
+            <div className="chat-wrapper">
+                <b>Enter your name:</b>
+                <textarea value={text} onChange={e => {
+                    setText(e.currentTarget.value)
+                }}></textarea>
+                <button onClick={() => {
+                    socket.emit('client-name-sent', text)
+                    setText('')
+                }}>Set name
+                </button>
+            </div>
+        )
+        : (
+            <div className="App">
+                <div className={'users'}>
+                    <p>Online:</p>
+                    {users.map((el, i) => {
+                        return (
+                            <b key={i}>{el} </b>
+                        )
+                    })}
+                </div>
+                <div className="chat-wrapper">
+                    <div className={'chat'}>
+                        {messages.map((el) => {
+                            return (<div key={el.id}>
+                                <b>{el.user.name}:</b> {el.message}
+                                <hr/>
+                            </div>)
+                        })}
+                    </div>
+                    <textarea value={text} onChange={e => setText(e.currentTarget.value)}></textarea>
+                    <button onClick={() => {
+                        socket.emit('client-message-sent', text)
+                        setText('')
+                    }}>Send
+                    </button>
+                </div>
+            </div>
+
+        );
 }
 
 export default App;
